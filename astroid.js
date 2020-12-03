7b44fc884a4c20c7c1967fef92b23d684751f400
Austin Esh <austinkentesh@gmail.com>
2020-12-03 16:06:34 -0500

    Make asteroids disappear on click

--- astroid.js
+++ astroid.js
@@ -12,7 +12,6 @@ const moveAsteroid = function() {
     for (ast of asteroids) {
         if (ast.growth >= 500) {
             removeAsteroid(ast);
-            console.log(health);
             health--;
             document.querySelector(".health").firstElementChild.textContent = health;
 
@@ -35,15 +34,6 @@ const moveAsteroid = function() {
             ast.a.style.left = ast.left + "px";
             ast.a.style.zIndex = ast.growth;
             ast.growth++;
-            if (ast.listener === "false") {
-              asteroid.a.addEventListener("click", destroy = () => {
-                console.log("i'm hit");
-                asteroid.a.removeEventListener("click", destroy);
-                ast.a.remove();
-                asteroids.splice(asteroids.indexOf(ast), 1);
-              });
-                ast.listener = "true";
-            }
         }
     }
 }
@@ -51,7 +41,7 @@ const moveAsteroid = function() {
 const makeAsteroid = function() {
     const rand = Math.random() * spawnArea.clientWidth;
     const rand2 = Math.random() * spawnArea.clientHeight;
-    asteroid = {a: document.createElement("img"), listener: "false", height: rand2, velocity: .01, left: rand, size: 1, growth: 0};
+    asteroid = {a: document.createElement("img"), height: rand2, velocity: .01, left: rand, size: 1, growth: 0};
     asteroid.a.setAttribute("src", "meteor.png");
     asteroid.a.style.width = 10 + "px";
     asteroid.a.style.height = 10 + "px";
@@ -60,6 +50,11 @@ const makeAsteroid = function() {
     asteroid.a.style.left = asteroid.left + "px";
     spawnArea.appendChild(asteroid.a);
     asteroids.push(asteroid);
+
+    const ast = asteroid;
+    ast.a.addEventListener("click", () => {
+        removeAsteroid(ast);
+    });
 }
 
 makeAsteroid();
